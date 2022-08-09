475c9adc3b85f72b0488089eaadaf07bebbe9e97
rwarner <kentoe@gmail.com>
2022-08-09 10:56:54 -0400

    Use service locator for our testing

--- Advanced - Part 2/Testing/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ Advanced - Part 2/Testing/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -7,15 +7,38 @@ import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Test
 import org.junit.runner.RunWith
 import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
 
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
 internal class TaskDetailFragmentTest {
+
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    // Reset repository after each test
+    @After
+    fun cleanupDb() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
     @Test
-    fun activeTaskDetails_DisplayedInUI() {
-        // GIVEN - Add active (incomplete task to the DB
+    fun activeTaskDetails_DisplayedInUI() = runBlockingTest {
+        // GIVEN - Add active (incomplete task to the DB)
         val activeTask = Task("Active Task", "AndroidX Rocks", false)
+
+        repository.saveTask(activeTask)
 
         // WHEN - Details fragment launched to display task
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
--- Advanced - Part 2/Testing/app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ Advanced - Part 2/Testing/app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
@@ -8,13 +9,17 @@ import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepo
 import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
+import kotlinx.coroutines.runBlocking
 
 object ServiceLocator {
+
+    private val lock = Any()
 
     private var database: ToDoDatabase? = null
 
     @Volatile
     var tasksRepository: TasksRepository? = null
+        @VisibleForTesting set
 
     fun provideTasksRepository(context: Context): TasksRepository {
         synchronized(this) {
@@ -41,4 +46,22 @@ object ServiceLocator {
         database = result
         return result
     }
+
+    // Only going to want to reset the repository in tests
+    @VisibleForTesting
+    fun resetRepository() {
+        synchronized(lock) {
+            runBlocking {
+                TasksRemoteDataSource.deleteAllTasks()
+            }
+            // Clear all data to avoid test pollution.
+            database?.apply {
+                clearAllTables()
+                close()
+            }
+            database = null
+            tasksRepository = null
+        }
+    }
+
 }
